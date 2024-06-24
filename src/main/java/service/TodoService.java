f16f8e6ceb7df9cec337a7711283e4f029ec1de9
joao <gabriel.122.gm85@gmail.com>
2024-06-23 21:38:22 -0300

    first commit

---
+++ src/main/java/service/TodoService.java
@@ -0,0 +1,38 @@
+package service;
+
+import entity.Todo;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import repository.TodoRepository;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository){
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list(){
+        Sort sort = Sort.by("priority").descending().and(Sort.by("name").descending());
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+
+}
